db2c2d6b7cc16c3ce79af8624d9fe5e5d3e1c63f
Atuan <jain60501@gmail.com.tw>
2023-03-26 17:04:27 +0800

    added flag to trigger down

--- Assets/Game/Prototype/PoseEventHandler.cs
+++ Assets/Game/Prototype/PoseEventHandler.cs
@@ -1,36 +1,3 @@
-﻿using Oculus.Interaction;
-using UnityEngine;
-
-namespace Game.Prototype{
-	public class PoseEventHandler : MonoBehaviour{
-		[SerializeField, Interface(typeof(ISelector))]
-		private MonoBehaviour fingerGunPoseWrapper;
-
-		[SerializeField, Interface(typeof(ISelector))]
-		private MonoBehaviour triggerPoseWrapper;
-
-		private ISelector _fingerGunPoseSelector;
-		private ISelector _triggerPoseSelector;
-		private bool _gunPose;
-		private bool _trigger;
-
-		private void Start(){
-			_fingerGunPoseSelector = fingerGunPoseWrapper as ISelector;
-			_triggerPoseSelector = triggerPoseWrapper as ISelector;
-			if(_fingerGunPoseSelector != null){
-				_fingerGunPoseSelector.WhenSelected += () => { _gunPose = true; };
-			}
-
-			if(_triggerPoseSelector != null){
-				_triggerPoseSelector.WhenSelected += () => { _trigger = true; };
-				_triggerPoseSelector.WhenUnselected += () => { _trigger = false; };
-			}
-		}
-
-		private void Update(){
-			if(_gunPose && _trigger){
-				Debug.Log($"Fire");
-			}
-		}
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5db836159bb862316ad83e8d73fcf0259c5f577d15849e2adb7f1d15d31ad5f
+size 1157
