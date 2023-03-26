a60588f5e8337c33e3816caf1f8a799a057430ca
Atuan <jain60501@gmail.com.tw>
2023-03-26 16:37:23 +0800

    set active pose with trigger using OR

--- Assets/Game/Prototype/PoseEventHandler.cs
+++ Assets/Game/Prototype/PoseEventHandler.cs
@@ -1,53 +1,36 @@
-﻿using System;
-using Oculus.Interaction;
+﻿using Oculus.Interaction;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Game.Prototype{
 	public class PoseEventHandler : MonoBehaviour{
 		[SerializeField, Interface(typeof(ISelector))]
 		private MonoBehaviour fingerGunPoseWrapper;
 
-		private ISelector _fingerGunPoseSelector;
-
 		[SerializeField, Interface(typeof(ISelector))]
 		private MonoBehaviour triggerPoseWrapper;
 
+		private ISelector _fingerGunPoseSelector;
 		private ISelector _triggerPoseSelector;
-
-		/// <summary>
-		/// State 
-		/// </summary>
 		private bool _gunPose;
-
 		private bool _trigger;
 
 		private void Start(){
 			_fingerGunPoseSelector = fingerGunPoseWrapper as ISelector;
 			_triggerPoseSelector = triggerPoseWrapper as ISelector;
-			this.AssertField(_fingerGunPoseSelector, nameof(_fingerGunPoseSelector));
-			this.AssertField(_triggerPoseSelector, nameof(_triggerPoseSelector));
 			if(_fingerGunPoseSelector != null){
-				_fingerGunPoseSelector.WhenSelected += () => FingerGunPoseActive(true);
-				_fingerGunPoseSelector.WhenUnselected += () => FingerGunPoseActive(false);
+				_fingerGunPoseSelector.WhenSelected += () => { _gunPose = true; };
 			}
 
 			if(_triggerPoseSelector != null){
-				_triggerPoseSelector.WhenSelected += () => TriggerPoseActive(true);
-				_triggerPoseSelector.WhenUnselected += () => TriggerPoseActive(false);
+				_triggerPoseSelector.WhenSelected += () => { _trigger = true; };
+				_triggerPoseSelector.WhenUnselected += () => { _trigger = false; };
 			}
 		}
 
-		private void FingerGunPoseActive(bool active){
-			_gunPose = active;
-		}
-
-		private void TriggerPoseActive(bool active){
-			_trigger = active;
-		}
-
 		private void Update(){
-			
+			if(_gunPose && _trigger){
+				Debug.Log($"Fire");
+			}
 		}
 	}
 }
