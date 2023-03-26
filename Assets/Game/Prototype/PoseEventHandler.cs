0be7b10ab00c70742707fc021f53b15ec428628b
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-03-26 16:03:40 +0800

    added PoseEventHandler.cs to handle gun pose state and trigger state

---
+++ Assets/Game/Prototype/PoseEventHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Oculus.Interaction;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game.Prototype{
+	public class PoseEventHandler : MonoBehaviour{
+		[SerializeField, Interface(typeof(ISelector))]
+		private MonoBehaviour fingerGunPoseWrapper;
+
+		private ISelector _fingerGunPoseSelector;
+
+		[SerializeField, Interface(typeof(ISelector))]
+		private MonoBehaviour triggerPoseWrapper;
+
+		private ISelector _triggerPoseSelector;
+
+		/// <summary>
+		/// State 
+		/// </summary>
+		private bool _gunPose;
+
+		private bool _trigger;
+
+		private void Start(){
+			_fingerGunPoseSelector = fingerGunPoseWrapper as ISelector;
+			_triggerPoseSelector = triggerPoseWrapper as ISelector;
+			this.AssertField(_fingerGunPoseSelector, nameof(_fingerGunPoseSelector));
+			this.AssertField(_triggerPoseSelector, nameof(_triggerPoseSelector));
+			if(_fingerGunPoseSelector != null){
+				_fingerGunPoseSelector.WhenSelected += () => FingerGunPoseActive(true);
+				_fingerGunPoseSelector.WhenUnselected += () => FingerGunPoseActive(false);
+			}
+
+			if(_triggerPoseSelector != null){
+				_triggerPoseSelector.WhenSelected += () => TriggerPoseActive(true);
+				_triggerPoseSelector.WhenUnselected += () => TriggerPoseActive(false);
+			}
+		}
+
+		private void FingerGunPoseActive(bool active){
+			_gunPose = active;
+		}
+
+		private void TriggerPoseActive(bool active){
+			_trigger = active;
+		}
+
+		private void Update(){
+			
+		}
+	}
+}
