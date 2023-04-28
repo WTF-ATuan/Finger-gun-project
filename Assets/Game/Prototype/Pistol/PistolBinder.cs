22c931e80020ffd68d29ef061339df339bf788d7
Atuan <jain60501@gmail.com.tw>
2023-04-28 15:09:38 +0800

    added simple haptic

--- Assets/Game/Prototype/Pistol/PistolBinder.cs
+++ Assets/Game/Prototype/Pistol/PistolBinder.cs
@@ -1,9 +1,16 @@
-﻿using InfimaGames.LowPolyShooterPack;
+﻿using System.Collections;
+using InfimaGames.LowPolyShooterPack;
 using UnityEngine;
 
 namespace Game.Prototype.Pistol{
 	public class PistolBinder : MonoBehaviour{
 		private Weapon _bindingWeapon;
+
+
+		// 設置震動參數
+		private const float Amplitude = 1.0f;
+		private const float Duration = 0.3f;
+		private const float Frequency = 300.0f;
 
 		private void Start(){
 			_bindingWeapon = GetComponent<Weapon>();
@@ -13,6 +20,7 @@ namespace Game.Prototype.Pistol{
 			var openingFire = OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger);
 			if(openingFire){
 				_bindingWeapon.Fire();
+				SimpleHaptic();
 			}
 
 			var reloading = OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger);
@@ -20,5 +28,20 @@ namespace Game.Prototype.Pistol{
 				_bindingWeapon.Reload();
 			}
 		}
+
+		private void SimpleHaptic(){
+			var hapticsClip = new OVRHapticsClip();
+			// 設置震動數據
+			var cnt = (int)(Duration * OVRHaptics.Config.SampleRateHz);
+			for(var i = 0; i < cnt; i++){
+				var time = Mathf.PI * 2.0f * Frequency * (i / (float)OVRHaptics.Config.SampleRateHz);
+				var sample = Amplitude * Mathf.Sin(time);
+				hapticsClip.WriteSample((byte)Mathf.RoundToInt(sample * 127 + 128));
+			}
+
+			var rightChannel = OVRHaptics.RightChannel;
+			rightChannel.Preempt(hapticsClip);
+		}
+		
 	}
 }
