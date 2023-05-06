8a41523f77c40b0230abcf803855c21c133a738c
Atuan <jain60501@gmail.com.tw>
2023-05-06 18:01:34 +0800

    added Pistol UI

---
+++ Assets/Game/Prototype/Pistol/PistolUIBinder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using uPIe;
+
+namespace Game.Prototype.Pistol{
+	[RequireComponent(typeof(uPIeMenu))]
+	public class PistolUIBinder : MonoBehaviour{
+		private uPIeMenu _menu;
+
+		private void Awake(){
+			_menu = GetComponent<uPIeMenu>();
+		}
+
+		private void Start(){
+			_menu.ControlWithGamepad = true;
+			_menu.UseCustomInputSystem = true;
+		}
+
+		private void Update(){
+			var leftPadInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+			_menu.CustomInput = leftPadInput;
+		}
+	}
+}
