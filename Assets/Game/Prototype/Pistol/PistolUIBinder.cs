35deeb815541c41044d7deed3ae5a0c749a42e36
Atuan <jain60501@gmail.com.tw>
2023-05-06 19:11:41 +0800

    finish spawn object with select UI

--- Assets/Game/Prototype/Pistol/PistolUIBinder.cs
+++ Assets/Game/Prototype/Pistol/PistolUIBinder.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using Oculus.Interaction.HandGrab;
+using UnityEngine;
 using uPIe;
 
 namespace Game.Prototype.Pistol{
 	[RequireComponent(typeof(uPIeMenu))]
 	public class PistolUIBinder : MonoBehaviour{
 		private uPIeMenu _menu;
+		[SerializeField] private GameObject grabbable;
+		[SerializeField] private HandGrabInteractor interactor;
 
 		private void Awake(){
 			_menu = GetComponent<uPIeMenu>();
@@ -16,6 +19,24 @@ namespace Game.Prototype.Pistol{
 		}
 
 		private void Update(){
+			Picking();
+			Selecting();
+		}
+
+		private void Selecting(){
+			var selected = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
+			if(!selected) return;
+			var selectedId = _menu.SelectedPieceId;
+			if(selectedId == 1){
+				var grabObject = Instantiate(grabbable, transform.position, Quaternion.identity);
+				var grabInteractable = grabObject.GetComponentInChildren<HandGrabInteractable>();
+				interactor.ForceSelect(grabInteractable);
+			}
+
+			_menu.Deselect();
+		}
+
+		private void Picking(){
 			var leftPadInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 			_menu.CustomInput = leftPadInput;
 		}
