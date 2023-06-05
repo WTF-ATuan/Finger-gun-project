5a80e3573bc77764ccca42390dab7498dc74c2b6
Atuan <jain60501@gmail.com.tw>
2023-06-05 15:16:42 +0800

    feature : scene of v0.1 and door gate

---
+++ Assets/Game/Prototype/Quick Gun - Single/DoorGate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Game.Prototype.Quick_Gun___Single{
+	public class DoorGate : MonoBehaviour{
+		[SerializeField] private GameObject leftGate;
+		[SerializeField] private GameObject rightGate;
+		[SerializeField] private float duration = 1;
+
+		private void OnEnable(){
+			OpenGate();
+		}
+
+		private void OnDisable(){
+			CloseGate();
+		}
+
+		public void OpenGate(){
+			leftGate.transform.DOMoveX(leftGate.transform.position.x +5, duration);
+			rightGate.transform.DOMoveX(rightGate.transform.position.x - 5, duration);
+		}
+
+		public void CloseGate(){
+			leftGate.transform.DOMoveX(leftGate.transform.position.x - 5, duration);
+			rightGate.transform.DOMoveX(rightGate.transform.position.x + 5, duration);
+		}
+	}
+}
