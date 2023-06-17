e72c11673e08a014b2ecdf354f94e8491e52437c
Atuan <jain60501@gmail.com.tw>
2023-06-17 15:11:55 +0800

    📌added left hand model and improve hit feeling
    - added hit sound (player , left hand)
    - added hit hand particle
    - change player hit effect

--- Assets/Game/Prototype/Quick Gun - Single/Art_object/DefensiveHand.cs
+++ Assets/Game/Prototype/Quick Gun - Single/Art_object/DefensiveHand.cs
@@ -3,14 +3,33 @@ using UnityEngine;
 
 namespace Game.Prototype.Quick_Gun___Single.Art_object{
 	public class DefensiveHand : MonoBehaviour{
-		private void Update(){
-			var leftHandGrip = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
-			if(leftHandGrip) ActiveDefensiveHand();
-			else DisableDefensiveHand();
+		/// <summary>
+		/// priority
+		/// - hit sound active
+		/// - play hit particle
+		/// - player hold grip can active and disable defensive hand
+		/// </summary>
+		[SerializeField] private AudioClip hitClip;
+
+		[SerializeField] private GameObject hitVFX;
+
+		private AudioSource _audioSource;
+
+		private void Start(){
+			_audioSource = gameObject.AddComponent<AudioSource>();
 		}
 
-		private void ActiveDefensiveHand(){ }
+		private void Update(){
+			var leftHandGrip = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
+		}
 
-		private void DisableDefensiveHand(){ }
+		private void OnTriggerEnter(Collider other){
+			if(!other.TryGetComponent(out Projectile projectile)) return;
+			_audioSource.PlayOneShot(hitClip);
+			var attachPoint = other.ClosestPoint(transform.position);
+			var vfxClone = Instantiate(hitVFX, attachPoint, Quaternion.identity);
+			Destroy(projectile.gameObject);
+			Destroy(vfxClone, 1.5f);
+		}
 	}
 }
--- Assets/Game/Prototype/Quick Gun - Single/HitBoxEffect.cs
+++ Assets/Game/Prototype/Quick Gun - Single/HitBoxEffect.cs
@@ -7,28 +7,40 @@ namespace Game.Prototype.Quick_Gun___Single{
 	public class HitBoxEffect : MonoBehaviour{
 		// Camera Red VFX 
 		// Controller Vibration
-		[SerializeField] private Image hitEffectImage;
+		[SerializeField] private RawImage hitEffectImage;
+		[SerializeField] private AudioClip hitSound;
+
+		private AudioSource _audioSource;
+
+		private void Start(){
+			_audioSource = gameObject.AddComponent<AudioSource>();
+		}
+
 		private void OnTriggerEnter(Collider other){
 			if(!other.TryGetComponent(out Projectile projectile)) return;
 			CameraHitEffect();
 			ControllerHitEffect();
+			_audioSource.PlayOneShot(hitSound);
 			Destroy(projectile.gameObject);
 		}
+
 		[Button]
 		private void CameraHitEffect(){
 			hitEffectImage.gameObject.SetActive(true);
-			hitEffectImage.transform.DOScale(0.45f, 0.4f)
+			hitEffectImage.DOFade(1 , 0.5f)
 					.OnComplete(() => {
 						hitEffectImage.gameObject.SetActive(false);
-						hitEffectImage.transform.localScale = Vector3.one * 0.15f;
+						hitEffectImage.DOFade(0.1f, 0);
 					});
 		}
+
 		[Button]
 		private void ControllerHitEffect(){
 			OVRInput.SetControllerVibration(1, 1f, OVRInput.Controller.RTouch);
 			OVRInput.SetControllerVibration(1, 1f, OVRInput.Controller.LTouch);
-			Invoke(nameof(StopHaptic) , 0.35f);
+			Invoke(nameof(StopHaptic), 0.35f);
 		}
+
 		private void StopHaptic(){
 			OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
 		}
