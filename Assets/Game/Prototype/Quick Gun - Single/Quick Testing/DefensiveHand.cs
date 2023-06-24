e2b0ccdcc83f701648f4c880c76eee4b54588865
Atuan <jain60501@gmail.com.tw>
2023-06-24 16:10:51 +0800

    added shield back to DefensiveHand

--- Assets/Game/Prototype/Quick Gun - Single/Quick Testing/DefensiveHand.cs
+++ Assets/Game/Prototype/Quick Gun - Single/Quick Testing/DefensiveHand.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Game.Prototype.Quick_Gun___Single.Art_object{
@@ -13,22 +16,46 @@ namespace Game.Prototype.Quick_Gun___Single.Art_object{
 
 		[SerializeField] private GameObject hitVFX;
 
+		[SerializeField] private GameObject shield;
+		[SerializeField] private GameObject emitter;
+
 		private AudioSource _audioSource;
+
+		private Vector3 _shieldOriginScale;
 
 		private void Start(){
 			_audioSource = gameObject.AddComponent<AudioSource>();
+			shield.OnTriggerExitAsObservable().Subscribe(OnShieldHit);
+			shield.OnTriggerStayAsObservable().Subscribe(OnShieldHitStay);
+			_shieldOriginScale = shield.transform.localScale;
+		}
+
+		private void OnShieldHitStay(Collider obj){
+			if(!obj.gameObject.TryGetComponent(out Projectile projectile)) return;
+			if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+				DOTween.Kill(obj.transform);
+				obj.transform.position = shield.transform.position;
+				obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+				obj.GetComponent<Rigidbody>().useGravity = false;
+			}
 		}
 
 		private void Update(){
-			var leftHandGrip = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
+			ActiveShieldByGrip();
 		}
 
-		private void OnTriggerEnter(Collider other){
+		private void ActiveShieldByGrip(){
+			var leftHandGrip = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
+			var shieldMinScale = _shieldOriginScale * 0.2f;
+			var lerpShieldScale = Vector3.Lerp(shieldMinScale, _shieldOriginScale, leftHandGrip);
+			shield.transform.localScale = lerpShieldScale;
+		}
+
+		private void OnShieldHit(Collider other){
 			if(!other.TryGetComponent(out Projectile projectile)) return;
 			_audioSource.PlayOneShot(hitClip);
-			var attachPoint = other.ClosestPoint(transform.position);
+			var attachPoint = other.ClosestPoint(shield.transform.position);
 			var vfxClone = Instantiate(hitVFX, attachPoint, Quaternion.identity);
-			Destroy(projectile.gameObject);
 			Destroy(vfxClone, 1.5f);
 		}
 	}
