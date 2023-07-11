605333da5d34d5083f07e1cb82860b5435073fd8
Atuan <jain60501@gmail.com.tw>
2023-07-11 18:16:02 +0800

    finish boss Data collection and simple condition

--- Assets/Game/Prototype/Quick Gun - Single/Stone Boss (Teleport)/StoneBoss.cs
+++ Assets/Game/Prototype/Quick Gun - Single/Stone Boss (Teleport)/StoneBoss.cs
@@ -1,11 +1,22 @@
-﻿using Sirenix.OdinInspector;
+﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.Prototype.Quick_Gun___Single.Stone_Boss__Teleport_{
 	public class StoneBoss : MonoBehaviour{
 		[Required] [SerializeField] private Transform player;
 
 		private Animator _animator;
+
+		[SerializeField] [MinMaxSlider(-180, 180, true)]
+		private Vector2 attackAngleRange = new(-45, 45);
+
+		[FormerlySerializedAs("attackRange")] [SerializeField]
+		private float attackDistance = 10;
+
+		private float _angleBetweenPlayer;
+		private float _distanceBetweenPlayer;
 
 
 		private void Start(){
@@ -20,10 +31,9 @@ namespace Game.Prototype.Quick_Gun___Single.Stone_Boss__Teleport_{
 		private void DetectAttackRange(){
 			var playerPosition = player.position;
 			var bossPosition = transform.position;
-			var distanceBetweenPlayer = Vector3.Distance(bossPosition, playerPosition);
 			var bossToPlayer = playerPosition - bossPosition;
-			var angleBetweenPlayer = Vector3.Dot(bossPosition, bossToPlayer);
-			Debug.Log($"angleBetweenPlayer = {angleBetweenPlayer} + {distanceBetweenPlayer}");
+			_distanceBetweenPlayer = Vector3.Distance(bossPosition, playerPosition);
+			_angleBetweenPlayer = Mathf.Atan2(bossToPlayer.z, bossToPlayer.x) * Mathf.Rad2Deg + 90;
 		}
 
 		private void UpdateAnimationState(){
@@ -34,7 +44,15 @@ namespace Game.Prototype.Quick_Gun___Single.Stone_Boss__Teleport_{
 		}
 
 		private bool NeedTurn(bool isRight){
-			return false;
+			var playerInAngle = attackAngleRange.x < _angleBetweenPlayer && _angleBetweenPlayer < attackAngleRange.y;
+			if(playerInAngle){
+				return false;
+			}
+
+			if(_angleBetweenPlayer < 0){
+				return isRight;
+			}
+			return !isRight;
 		}
 
 		private bool IsDizzy(){
@@ -42,7 +60,23 @@ namespace Game.Prototype.Quick_Gun___Single.Stone_Boss__Teleport_{
 		}
 
 		private bool CanThrow(){
-			return false;
+			var playerInAngle = attackAngleRange.x < _angleBetweenPlayer && _angleBetweenPlayer < attackAngleRange.y;
+			var playerInDistance = _distanceBetweenPlayer > attackDistance;
+			return playerInAngle && playerInDistance;
+		}
+
+		private void OnDrawGizmos(){
+			if(!player) return;
+			Gizmos.color = Color.green;
+			var position = transform.position;
+			var minAngle = (attackAngleRange.x - 90) * Mathf.Deg2Rad;
+			var minX = position.x + attackDistance * Mathf.Cos(minAngle);
+			var minZ = position.z + attackDistance * Mathf.Sin(minAngle);
+			Gizmos.DrawLine(position, new Vector3(minX, position.y, minZ));
+			var maxAngle = (attackAngleRange.y - 90) * Mathf.Deg2Rad;
+			var maxX = position.x + attackDistance * Mathf.Cos(maxAngle);
+			var maxZ = position.z + attackDistance * Mathf.Sin(maxAngle);
+			Gizmos.DrawLine(position, new Vector3(maxX, position.y, maxZ));
 		}
 	}
 }
