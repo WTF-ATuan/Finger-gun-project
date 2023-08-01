c511435bf699a775af6c3efde744557336524a78
Atuan <jain60501@gmail.com.tw>
2023-08-01 18:32:52 +0800

    added portal feature

---
+++ Assets/Game/Prototype/Smooth_Portal/PortalCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Prototype.Smooth_Portal{
+	public class PortalCamera : MonoBehaviour{
+		public Transform playerCamera;
+		public Transform portal;
+		public Transform otherPortal;
+
+
+		private void LateUpdate(){
+			var playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+			transform.position = portal.position + playerOffsetFromPortal;
+
+			var angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+			var portalRotationsDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+			var needFaceToDirection = portalRotationsDifference * playerCamera.forward;
+			transform.rotation = Quaternion.LookRotation(needFaceToDirection, Vector3.up);
+		}
+	}
+}
---
+++ Assets/Game/Prototype/Smooth_Portal/PortalTextureSetup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game.Prototype.Smooth_Portal{
+	public class PortalTextureSetup : MonoBehaviour{
+		public Camera cameraB;
+		public Material cameraMatB;
+
+		private void Start(){
+			if(cameraB.targetTexture != null){
+				cameraB.targetTexture.Release();
+			}
+
+			cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+			cameraMatB.mainTexture = cameraB.targetTexture;
+		}
+	}
+}
