4821f634be1005f6ed972dd0caf45d4c9cce9583
Atuan <jain60501@gmail.com.tw>
2023-08-18 14:31:18 +0800

    Breakable stone blocker

--- Assets/Game/Prototype/Quick Gun - Single/Quick Testing/BreakableTarget.cs
+++ Assets/Game/Prototype/Quick Gun - Single/Quick Testing/BreakableTarget.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 namespace Game.Prototype.Quick_Gun___Single{
 	public class BreakableTarget : MonoBehaviour{
 		[SerializeField] private int destroyCount = 1;
-		private int _hitCount;
+		private int _hitCount = 0;
 		[SerializeField] private GameObject brokenVFX;
 
 
 		public void OnCollisionEnter(Collision collision){
 			if(!collision.gameObject.TryGetComponent(out Projectile projectile)) return;
 			_hitCount++;
-			if(_hitCount >= destroyCount){
-				EventAggregator.Publish(new TargetHitEvent());
-				transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => Destroy(gameObject));
-				if(!brokenVFX) return;
-				var vfxClone = Instantiate(brokenVFX, transform.position, Quaternion.identity);
-				Destroy(vfxClone, 1.5f);
-			}
+			if(_hitCount < destroyCount) return;
+			EventAggregator.Publish(new TargetHitEvent());
+			transform.DOScale(Vector3.zero, 0.5f).OnComplete(() => Destroy(gameObject));
+			if(!brokenVFX) return;
+			var vfxClone = Instantiate(brokenVFX, transform.position, Quaternion.identity);
+			Destroy(vfxClone, 1.5f);
 		}
 	}
 
