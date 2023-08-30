d270494b62171ebb9388b5a664f5d4fb802a0f77
Atuan <jain60501@gmail.com.tw>
2023-08-30 14:19:42 +0800

    fixed bug of Dodge Ability

--- Assets/Game/Prototype/SubstituteFeature/DodgeAbility.cs
+++ Assets/Game/Prototype/SubstituteFeature/DodgeAbility.cs
@@ -1,27 +1,32 @@
 ﻿using System.Collections.Generic;
 using Game.Prototype.Pistol;
 using Game.Prototype.Quick_Gun___Single.Stone_Boss__Teleport_;
+using Sirenix.OdinInspector;
 using UniRx;
 using UnityEngine;
 
 namespace Game.Prototype.SubstituteFeature{
-	public class DodgeAbility : MonoBehaviour{
-		[SerializeField] private List<ShotGunBinder> effectComponent;
-		[SerializeField] private ParticleSystem abilityVfx;
-		[SerializeField] private AudioClip soundEffect;
+    public class DodgeAbility : MonoBehaviour{
+        [SerializeField] private List<ShotGunBinder> effectComponent;
+        [SerializeField] private ParticleSystem abilityVfx;
+        [SerializeField] private AudioClip soundEffect;
 
-		private AudioSource _audioSource;
+        private AudioSource _audioSource;
 
 
-		private void Start(){
-			EventAggregator.OnEvent<DodgeAction>().Subscribe(OnDodge);
-			_audioSource = gameObject.GetComponent<AudioSource>();
-		}
+        private void Start(){
+            EventAggregator.OnEvent<DodgeAction>().Subscribe(OnDodge);
+            _audioSource = gameObject.GetComponent<AudioSource>();
+        }
+        [Button]
+        private void Test(){
+            OnDodge(new DodgeAction());
+        }
 
-		private void OnDodge(DodgeAction obj){
-			effectComponent.ForEach(x => x.ModifyCurrentAmmo(x.ammoMax));
-			abilityVfx.Play();
-			_audioSource.PlayOneShot(soundEffect);
-		}
-	}
+        private void OnDodge(DodgeAction obj){
+            effectComponent.ForEach(x => x.ModifyCurrentAmmo(x.ammoMax));
+            abilityVfx.Play();
+            _audioSource.PlayOneShot(soundEffect);
+        }
+    }
 }
