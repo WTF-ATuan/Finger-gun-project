063877c6b26a384bb9e216dcb9c7ca10a5d26e71
Atuan <jain60501@gmail.com.tw>
2023-09-21 16:51:35 +0800

    modify android permission add firebase event

--- Assets/Game/Prototype/Project Update/FirebaseSetup.cs
+++ Assets/Game/Prototype/Project Update/FirebaseSetup.cs
@@ -1,19 +1,36 @@
-﻿using System;
-using Firebase;
+﻿using Firebase;
 using UnityEngine;
 
 namespace Game.Prototype.Project_Update{
 	public class FirebaseSetup : MonoBehaviour{
 		private FirebaseApp _app;
+
 		private void Start(){
 			FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
 				var dependencyStatus = task.Result;
-				if (dependencyStatus == DependencyStatus.Available) {
+				if(dependencyStatus == DependencyStatus.Available){
 					_app = FirebaseApp.DefaultInstance;
-				} else {
+				}
+				else{
 					Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
 				}
 			});
+		}
+
+		public void PlayerDeadEvent(){
+			Firebase.Analytics.FirebaseAnalytics.LogEvent("player_dead");
+		}
+
+		public void PassStageEvent(int stageCount = 1){
+			Firebase.Analytics.FirebaseAnalytics.LogEvent("pass_stage", "stage", $"{stageCount}");
+		}
+
+		public void BeatBossEvent(){
+			Firebase.Analytics.FirebaseAnalytics.LogEvent("beat_boss");
+		}
+
+		public void ReplayEvent(){
+			Firebase.Analytics.FirebaseAnalytics.LogEvent("replay_game");
 		}
 	}
 }
