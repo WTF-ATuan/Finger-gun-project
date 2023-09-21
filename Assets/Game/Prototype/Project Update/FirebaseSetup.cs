481953c54a56fc417de768befa1948be36f10c9b
Atuan <jain60501@gmail.com.tw>
2023-09-21 14:53:20 +0800

    added firebase set up feature

---
+++ Assets/Game/Prototype/Project Update/FirebaseSetup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Firebase;
+using UnityEngine;
+
+namespace Game.Prototype.Project_Update{
+	public class FirebaseSetup : MonoBehaviour{
+		private FirebaseApp _app;
+		private void Start(){
+			FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+				var dependencyStatus = task.Result;
+				if (dependencyStatus == DependencyStatus.Available) {
+					_app = FirebaseApp.DefaultInstance;
+				} else {
+					Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+				}
+			});
+		}
+	}
+}
