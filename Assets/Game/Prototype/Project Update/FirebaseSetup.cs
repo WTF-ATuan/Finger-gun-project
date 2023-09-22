830d587d0d471874b825dd5d450de6f440ecfe91
Atuan <jain60501@gmail.com.tw>
2023-09-22 19:11:14 +0800

    event upload

--- Assets/Game/Prototype/Project Update/FirebaseSetup.cs
+++ Assets/Game/Prototype/Project Update/FirebaseSetup.cs
@@ -1,19 +1,26 @@
 ﻿using Firebase;
 using Firebase.Analytics;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Game.Prototype.Project_Update{
 	public class FirebaseSetup : MonoBehaviour{
 		private FirebaseApp _app;
+
+		[SerializeField] private Text debugText;
 
 		private void Start(){
 			FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
 				var status = task.Result;
 				if(status == DependencyStatus.Available){
 					_app = FirebaseApp.DefaultInstance;
+					FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventLogin);
 					FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventTutorialBegin);
+					FirebaseAnalytics.LogEvent(FirebaseAnalytics.EventBeginCheckout);
+					debugText.text = $"{_app.Options.ProjectId}";
 				}
 				else{
+					debugText.text = $"Could not resolve all Firebase dependencies: {status}";
 					Debug.LogError($"Could not resolve all Firebase dependencies: {status}");
 				}
 			});
